7ac41087b502b3aa5d441134aecd5430a43d46ee
Leonardo Fontes <moreirafontesleo@gmail.com>
2021-01-11 11:36:33 -0300

    Made it easier to see what's happening

--- egg's keygenme - complex validation/sanitized_keygen.c
+++ egg's keygenme - complex validation/sanitized_keygen.c
@@ -56,10 +56,10 @@ int main(int argc,char **argv){
                     double a=pow(serialKey[19], serialKey[1]);
                     double b=pow(serialKey[0], serialKey[2]);
 
-                    puts("(a-b<0.0):                                                "itob((a-b<0.0)));
-                    puts("(0<serialKey[0] - serialKey[19]):                         "itob((0<serialKey[0] - serialKey[19])));
-                    puts("(serialKey[12]+serialKey[7]<140):                         "itob((serialKey[12]+serialKey[7]<140)));
-                    puts("(serialKey[10]*serialKey[8]<=serialKey[16]*serialKey[4]): "itob((serialKey[10]*serialKey[8]<=serialKey[16]*serialKey[4])));
+                    printf("(a-b<0.0):                                                %s\n", itob((a-b<0.0)));
+                    printf("(0<serialKey[0] - serialKey[19]):                         %s\n", itob((0<serialKey[0] - serialKey[19])));
+                    printf("(serialKey[12]+serialKey[7]<140):                         %s\n", itob((serialKey[12]+serialKey[7]<140)));
+                    printf("(serialKey[10]*serialKey[8]<=serialKey[16]*serialKey[4]): %s\n", itob((serialKey[10]*serialKey[8]<=serialKey[16]*serialKey[4])));
 
 
                     if (((a-b<0.0) && (0<serialKey[0] - serialKey[19]) &&
